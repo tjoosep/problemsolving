b7c96f5a4a46478c86cc301c4c6f95a147fa3367
Tanel Joosep <tanel.joosep@cyber.ee>
2020-02-25 12:01:56 +0200

    dice rolling beginner task

---
+++ src/com/impro/codeabbey/beginner/DiceRolling.java
@@ -0,0 +1,21 @@
+package com.impro.codeabbey.beginner;
+
+import java.util.*;
+
+public class DiceRolling {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int dice = 6;
+
+        // floor(x * (B-A) + A)
+        // x * 6 + 1
+        for (int i = 0; i < n; i++) {
+            float input = in.nextFloat();
+            float a = input * dice;
+            int b = (int) a;
+            System.out.print(b + 1 + " ");
+        }
+    }
+}
