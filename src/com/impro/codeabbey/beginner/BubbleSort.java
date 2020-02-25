cb586d4a88e85b9831fb931e9792288a5d260298
Tanel Joosep <tanel.joosep@cyber.ee>
2020-02-25 16:26:08 +0200

    some more problems done

--- src/com/impro/codeabbey/beginner/BubbleSort.java
+++ src/com/impro/codeabbey/beginner/BubbleSort.java
@@ -5,13 +5,39 @@ import java.util.*;
 
 public class BubbleSort {
     public static void main(String[] args) {
+        /*
         Scanner in = new Scanner(System.in);
         String[] input = in.nextLine().split(" ");
         long[] arr = new long[input.length-1];
         for (int i = 0; i < input.length-1; i++) {
             arr[i] = Integer.parseInt(input[i]);
         }
-    }
 
+         */
+        //15, 18, 13, 12, 2, 9, 6, 7, 5, 8, 4, 3, 14, 1, 11, 10, 17, 16
+        //14 83
+        int[] a = new int[] {15, 18, 13, 12, 2, 9, 6, 7, 5, 8, 4, 3, 14, 1, 11, 10, 17, 16};
+        bubbleSort(a);
+
+    }
+    
+    static void bubbleSort(int[] arr) {
+        int swaps = 0;
+        int passes = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] <= arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    swaps++;
+                }
+            }
+            passes++;
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.print(swaps + " " + passes + " ");
+    }
 
 }
--- src/com/impro/codeabbey/beginner/ModuloAndTimeDiff.java
+++ src/com/impro/codeabbey/beginner/ModuloAndTimeDiff.java
@@ -1,5 +1,34 @@
 package com.impro.codeabbey.beginner;
 
 
+import java.util.*;
+
+
 public class ModuloAndTimeDiff {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            String[] times = in.nextLine().split(" ");
+            int[] time1 = new int[times.length/2];
+            int[] time2 = new int[times.length/2];
+            for (int j = 0; j < 4; j++) {
+                time1[j] = Integer.parseInt(times[j]);
+                time2[j] = Integer.parseInt(times[j + 4]);
+            }
+            calcTimeDifferences(time1, time2);
+        }
+    }
+
+    static void calcTimeDifferences(int[] time1, int[] time2) {
+        int days = time2[0] - time1[0];
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+
+        System.out.print("(" + days + " " + hours + " " + minutes + " " + seconds + ")");
+    }
 }
